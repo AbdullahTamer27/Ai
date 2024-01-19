1028fd33470a8466effb6d7b79cc4996636256f1
Eyad Elkiey <eyadelkiey@hotmail.com>
2024-01-19 03:05:30 +0200

    Update alpha_beta.py

--- MINMAX/alpha_beta.py
+++ MINMAX/alpha_beta.py
@@ -27,3 +27,22 @@ def alphabeta (position, depth, max_player, alpha, beta):
                 best_move = move
         return alpha , best_move
 
+ else:
+        best_move = None
+
+        for move in get_all_movies(position, Black, ):
+            evaluation = alphabeta(move, depth - 1, True, alpha, beta)
+            if (evaluation == -1):
+                minval = beta
+            else:
+                minval = min(beta, evaluation)
+
+            if(alpha >= minval):
+                #cutoff
+                beta = -1
+                break
+            else:
+                beta = minval
+            if beta == evaluation:
+                best_move = move
+        return beta, best_move
